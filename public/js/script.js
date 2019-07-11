0499e52dcc39cf33592935ad8817262d301deb0a
Sahar Adem <sahar.adem95@gmail.com>
2019-07-11 06:52:17 +0300

    adding universities links

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,56 @@
 const search = document.getElementById("search");
 search.addEventListener("keyup", event => {
-    console.log(event.target.value);
-    fetch(`/search?q=${event.target.value}`)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            let listItem = document.getElementById("list");
-            let ul = document.createElement("ul");
-            listItem.appendChild(ul)
+  console.log(event.target.value);
+  fetch(`/search?q=${event.target.value}`)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      let listItem = document.getElementById("list");
+      let ul = document.createElement("ul");
+      listItem.appendChild(ul);
 
-            console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        let li = document.createElement("li");
+        li.setAttribute("class", "item");
+        const valueOfArr = document.createTextNode(data[i]);
+        li.appendChild(valueOfArr);
+        ul.appendChild(li);
+      }
 
-            for (let i = 0; i < data.length; i++) {
+      list.textContent = "";
+      list.appendChild(ul);
+    });
+});
 
-                let li = document.createElement("li");
-                li.setAttribute("class", "item");
-                const valueOfArr = document.createTextNode(data[i]);
-                li.appendChild(valueOfArr);
-                ul.appendChild(li);
+let button = document.getElementById("button");
 
-            }
+button.addEventListener("click", () => {
+  fetch(
+    `https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json`
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      let y = search.value;
+      const answer = data.map(function(x) {
+        if (x.name === y) {
+          return x.web_pages;
+        }
+      });
+      const answer2 = answer.filter(x => x != undefined);
 
-            list.textContent = "";
-            list.appendChild(ul);
-            // for (let i = 0; i < data.length; i++) {
-            //     console.log(data[i]);
+      const div = document.getElementById("universitiesLink");
+      div.setAttribute(
+        "style",
+        "  background-color: ghostwhite ;text-align: center; font-size: 20px; margin-top: 20px; padding: 20px;"
+      );
 
-            //     let item = document.createElement("li");
-            //     item = data[i];
-            //     ul.appendChild(item);
-            // }
-        });
+      const anchor = document.createElement("A");
+      const links = document.createTextNode(search.value);
+      anchor.setAttribute("href", answer2);
+      anchor.appendChild(links);
+      div.appendChild(anchor);
+    });
 });
